3fe54f9e56b047d529ac693b0666ab74324eac89
Jere-Joonas Valtanen <jerevaltanen@gmail.com>
2025-09-18 23:06:09 +0300

    changes to color scheme. Added underline to active path link

--- src/components/nav/navItem.tsx
+++ src/components/nav/navItem.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import type { NavItem } from "./navItems";
 import styles from "@/components/nav/nav.module.css";
-import Text from "../text/text";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 type NavItemProps = {
   item: NavItem;
 };
 
 export default function NavItem({ item }: NavItemProps) {
+  const pathName = usePathname();
+
+  console.log(pathName);
+
   return (
-    <div className={styles["nav-item"]}>
+    <div
+      className={clsx(
+        styles["nav-item"],
+        pathName === item.href && styles["nav-item-current"]
+      )}
+    >
       <Link href={item.href}>{item.label}</Link>
     </div>
   );
